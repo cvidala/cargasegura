1fd5dbee367671c933c8be9ddb8a16972b90248c
Carlos Vidal <c.vidalaedo@gmail.com>
2026-06-07 02:32:40 -0400

    fix: iconos PNG 192/512, id y prefer_related_applications en manifest

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const CACHE = 'cargasegura-v1';
+const CACHE = 'cargasegura-v2';
 const ASSETS = [
     './',
     './index.html',
     './manifest.json',
-    './icon.svg'
+    './icon.svg',
+    './icon-192.png',
+    './icon-512.png'
 ];
 
 self.addEventListener('install', e => {
